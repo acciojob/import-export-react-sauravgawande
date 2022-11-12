7b21468127247913cd46a8ad7c526664896631c0
sauravgawande <gawandesaurav02@gmail.com>
2022-11-12 11:41:28 +0530

    firstcommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import SubmitButton from "./SubmitButton";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <SubHeading/>
+      <InputQuery />
+      <SubmitButton/>
     </div>
   )
 }
-
+export default App
 
